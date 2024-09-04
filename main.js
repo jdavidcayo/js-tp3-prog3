e1215d995687be02cbb5f2cbec39b969214fc5b2
J David Cayo <jdavidcayo@gmail.com>
2024-09-04 16:17:53 -0300

    objetos

--- main.js
+++ main.js
@@ -1,24 +1,98 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const sumaEjercicio2 = () => {
+  let a, b, c;
+  a = 5;
+  b = 10;
+  c = a + b;
+  console.log("La suma de a y b es:", c);
+}
 
-setupCounter(document.querySelector('#counter'))
+const ejercicio3 = () => {
+  let nombre;
+  nombre = prompt("¿Cual es tu nombre?");
+  console.log("Hola, ", nombre);
+}
+
+const operadoresLogicosYCondicionales = () => {
+  let a, b, c;
+  a = 40;
+  b = 90;
+  if(a > b) c=a;
+  else c=b;
+  console.log("El numero mayor es:", c);
+}
+
+const parImpar = () => {
+  let a = prompt("Ingresa un numero y te dire si es par o impar");
+
+  a = Number.parseInt(a);
+  if(a && a%2 === 0) console.log(`El numero ${a} es par`) ;
+  else if( a && a%2 != 0)console.log(`El numero ${a} es impar.`)
+  else console.log(`No es un numero valido.`)
+}
+
+const iteracion = () => {
+  let a = 10;
+  while( a >= 0){
+    console.log(a)
+    a--;
+  }
+}
+
+const doWhile = () => {
+  let a;
+  do{
+    a = prompt("Ingrese un numero mayor a 100:")
+    a = Number.parseInt(a)
+  }while(a <= 100);
+  console.log("Ingresaste un numero mayor a 100:", a);
+}
+
+function esPar(numero) {
+  return (numero%2 == 0)
+}
+
+const ejercicioEsPar = () => {
+  console.log("El numero 8 es par:", esPar(8))
+  console.log("El numero 7 es par:", esPar(7))
+}
+
+const convertirCelsiusAFahrenheit = (gradosCelcius) => {
+  return ((gradosCelcius * 1.8) + 32);
+}
+
+const objetos = () => {
+  const persona = {
+    nombre: "David",
+    edad: 36,
+    ciudad: "Barcelona",
+    cambiarCiudad: function (nuevaCiudad){this.ciudad = nuevaCiudad}
+  }
+  console.log(`Nombre: ${persona.nombre}. Edad: ${persona.edad}, Ciudad: ${persona.ciudad}`);
+  persona.cambiarCiudad("New York");
+  console.log(`Nombre: ${persona.nombre}. Edad: ${persona.edad}, Ciudad: ${persona.ciudad}`);
+}
+
+sumaEjercicio2();
+console.log("-------------------");
+// ejercicio3();
+console.log("-------------------");
+operadoresLogicosYCondicionales();
+console.log("-------------------");
+// parImpar();
+console.log("-------------------");
+// iteracion();
+console.log("-------------------");
+// doWhile();
+console.log("-------------------");
+// ejercicioEsPar();
+console.log("-------------------");
+console.log("30ºC son equivalentes a", convertirCelsiusAFahrenheit(30), "ºF");
+console.log("-------------------");
+objetos();
+
+
+
+
+
